762310496d3e67785440b240a838501d0df616b8
Sava Rizky Anugerah <savahp123@gmail.com>
2026-08-31 08:48:08 +0700

    feat: relate pengguna to polymorphic media

--- app/Models/Pengguna.php
+++ app/Models/Pengguna.php
@@ -4,13 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Pengguna extends Model
 {
     use HasFactory;
+
     protected $fillable = [
-        'nama', 
-        'email', 
+        'nama',
+        'email',
         'peran'
     ];
+
+    public function media(): MorphMany
+    {
+        return $this->morphMany(Media::class, 'mediable');
+    }
 }
