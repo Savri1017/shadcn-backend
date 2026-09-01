9ef1a67b98c1af9dd2bfe84b30613b0275df7b44
Sava Rizky Anugerah <savahp123@gmail.com>
2026-09-01 08:25:15 +0700

    feat: add polymorphic media routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PenggunaController;
 use App\Http\Controllers\JabatanController;
+use App\Http\Controllers\MediaController;
 
 Route::apiResource('penggunas', PenggunaController::class);
 Route::apiResource('jabatans', JabatanController::class)->only(['index', 'store']);
+
+Route::get('media/pengguna/{pengguna}', [MediaController::class, 'indexForPengguna']);
+Route::post('media/pengguna/{pengguna}', [MediaController::class, 'storeForPengguna']);
+Route::delete('media/{media}', [MediaController::class, 'destroy']);
